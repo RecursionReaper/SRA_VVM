93e4de733b690e6a70ac25b209dcc5bf14e0d57f
Kartikey Pathak <kartikeypathak.imp@gmail.com>
2024-08-03 14:17:20 +0530

    Add files via upload

---
+++ Sim.launch.py
@@ -0,0 +1,42 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+
+
+    package_name='vvm_gazebo' 
+
+    rsp = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','gazebo.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true'}.items()
+    )
+
+    
+    gazebo = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')]),
+             )
+
+    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
+                        arguments=['-topic', 'robot_description',
+                                   '-entity', 'vvm_gazebo'],
+                        output='screen')
+
+
+
+    return LaunchDescription([
+        rsp,
+        gazebo,
+        spawn_entity,
+    ])
