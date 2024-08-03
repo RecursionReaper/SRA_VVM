de4a1c2355a50ace2565117f722f350e7c567f99
Yash Suthar <ytsuthar28@gmail.com>
2024-08-03 15:18:39 +0530

    Iteration_3

--- simulation.launch.py
+++ simulation.launch.py
@@ -8,35 +8,50 @@ from launch_ros.actions import Node
 from launch.event_handlers import (OnProcessStart, OnProcessExit)
 from launch_ros.descriptions import ParameterValue
 import xacro
-
+ 
 
 # this is the function launch  system will look for
 def generate_launch_description():
     ####### DATA INPUT ##########
     xacro_file = 'open_manipulator_robot.urdf.xacro'
-    #xacro_file = "box_bot.xacro"robot_description
-    package_description = "simulation_vvm"
-    ####### DATA INPUT END ##########
-    config = os.path.join( get_package_share_directory('simulation_vvm'),
-    'config',
-    'vvm.yaml'
-    )
     
-    xacro_path = os.path.join(get_package_share_directory(package_description), "urdf", xacro_file)
-    # assert os.path.exists(xacro_path), f"The file {xacro_file} does not exist in {os.path.dirname(xacro_path)}"
-       
+    package_description = 'simulation_vvm'
+    ####### DATA INPUT END ##########
+    config = os.path.join(get_package_share_directory(package_description),'config','vvm.yaml')
+ 
+
+    # Earlier approach:
+    # assert os.path.exists(xacro_path), f"The file {xacro_file} does not exist in {os.path.dirname(xacro_path)}"  
     # urdf_content = xacro.process_file(xacro_path)
     # robot_description_content = urdf_content.toxml()
-    doc = xacro.parse(open(xacro_file))
-    xacro.process_doc(doc, mappings={'config_path': config})
-    robot_description_content = doc.toxml()
+
+    # MOdified:
+    # xacro_path = os.path.join(get_package_share_directory(package_description), "urdf", xacro_file)
+    # doc = xacro.parse(open(xacro_file))
+    # xacro.process_doc(doc, mappings={'config_path': config})
+    # robot_description_content = doc.toxml()
+    # robot_description = {"robot_description": robot_description_content}
     
-    robot_description = {"robot_description": robot_description_content}
-    
+    #Latest
+    xacro_path = os.path.join(get_package_share_directory(package_description), 'urdf/', xacro_file)    
+    assert os.path.exists(xacro_path), "The box_bot.xacro doesnt exist in "+str(xacro_path)
+
+    robot_description_config = xacro.process_file(xacro_path)
+    robot_description_content = robot_description_config.toxml()
+
+    print(robot_description_content)
+    spawn_node = Node(
+        package='simulation_vvm', 
+        executable='spawn_node.py', 
+        arguments=[robot_description_content], 
+        output='screen',
+    )
+
     control_node = Node(
         package="controller_manager",
         executable="ros2_control_node",
-        parameters=[robot_description, config],
+        parameters=[{"robot_description": robot_description_content}, 
+                    config],
         output="both", 
     )
 
@@ -58,7 +73,7 @@ def generate_launch_description():
         executable='robot_state_publisher',
         name='robot_state_publisher',
         emulate_tty=True,
-        parameters=[{'use_sim_time': True, 'robot_description': ParameterValue(Command(['xacro ',xacro_path]), value_type=str)}],
+        parameters=[{'use_sim_time': True, 'robot_description': ParameterValue(Command(['xacro ',xacro_path]), value_type=None)}],
         output="screen"
     )
 
@@ -66,7 +81,7 @@ def generate_launch_description():
     
     # Position and orientation
     # [X, Y, Z]
-    position = [0.274, 0.0, 0.205]
+    position = [0.0, 0.0, 0.0]
     # [Roll, Pitch, Yaw]
     orientation = [0.0, 0.0, 0.0]
     # Base Name or robot
@@ -114,8 +129,12 @@ def generate_launch_description():
 
     return LaunchDescription([  
         spawn_robot,
-        control_node_after_gazebo,
+        # spawn_node,
         robot_state_publisher_node,
-        delay_joint_state_broadcaster_spawner_after_spawn_robot,
-        delay_robot_controller_spawner_after_joint_state_broadcaster_spawner
+        # control_node_after_gazebo,
+        # delay_joint_state_broadcaster_spawner_after_spawn_robot,
+        # delay_robot_controller_spawner_after_joint_state_broadcaster_spawner
     ])
+
+
+   
