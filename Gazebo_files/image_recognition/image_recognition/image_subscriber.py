78f477e155b1c47097ad35a964b465a0637c3fad
NoobMaster-version <kartikeypathak.imp@gmail.com>
2024-08-30 03:29:25 +0530

    Input for image detection

---
+++ Gazebo_files/image_recognition/image_recognition/image_subscriber.py
@@ -0,0 +1,42 @@
+import rclpy
+from rclpy.node import Node
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
+
+
+class ImageSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('image_subscriber')
+        self.subscription = self.create_subscription(
+            Image,
+            '/Intel_Realsense/image_raw',
+            self.listener_callback,
+            10)
+        self.subscription 
+
+        self.br = CvBridge()
+
+    def listener_callback(self, msg):
+        self.get_logger().info('Receiving image')
+
+        current_frame = self.br.imgmsg_to_cv2(msg)       
+        cv2.imshow("Camera Feed", current_frame)
+
+        cv2.waitKey(1)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    image_subscriber = ImageSubscriber()
+
+    rclpy.spin(image_subscriber)
+
+    image_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
---
+++ Gazebo_files/image_recognition/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'image_recognition'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=[
+        'setuptools',
+        'rclpy',           
+        'opencv-python',   
+        'cv_bridge',
+    ],
+    zip_safe=True,
+    maintainer='kartikey',
+    maintainer_email='kartikeypathak.imp@gmail.com',
+    description='Package for image recognition using ROS 2 and OpenCV',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'image_subscriber = image_recognition.image_subscriber:main',
+        ],
+    },
+)
+
